d9359e8dbda5875d922e1136e8a40c1e9ab17caf
umbrielpost <umbrielpost@gmail.com>
2015-11-12 16:12:36 +0100

    Graan en Herder scripts etc

---
+++ Magic Sheppard/Assets/Scripts/GraanScript.cs
@@ -0,0 +1,58 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class GraanScript : MonoBehaviour {
+    public int aantalgraangekocht = 2;
+    public Text AantalGraanText;
+
+    private bool graantje = true;
+
+
+    // Use this for initialization
+    void Start () {
+        SetAantalGraanText();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		var H = GameObject.FindGameObjectWithTag ("Herder");
+		if (aantalgraangekocht > 0 && Input.GetKeyDown("space") && graantje == true)
+		{
+            transform.parent = null;
+            Vector3 GraanPlek = new Vector3(H.transform.position.x, H.transform.position.y, H.transform.position.z);
+			transform.position = GraanPlek;
+            graantje = false;
+			StartCoroutine(TimerGraan());
+		}
+}
+
+	IEnumerator TimerGraan()
+	{
+        //Hier komt de aantrekking van het graan voor de schapen
+        aantalgraangekocht = aantalgraangekocht - 1;
+        SetAantalGraanText();
+        yield return new WaitForSeconds (10);
+		gameObject.SetActive (false);
+        if (aantalgraangekocht > 0)
+        {
+            ReturnBeginGraan();
+        }
+	}
+
+    void ReturnBeginGraan()
+    {
+        gameObject.SetActive(true);
+        var H = GameObject.FindGameObjectWithTag("Herder");
+        Vector3 beginplekgraan = new Vector3(H.transform.position.x, H.transform.position.y, H.transform.position.z);
+        transform.position = beginplekgraan;
+        transform.parent = H.transform;
+        graantje = true;
+    }
+
+    void SetAantalGraanText()
+    {
+        AantalGraanText.text = "Graan: " + aantalgraangekocht;
+    }
+
+}
--- Magic Sheppard/Assets/Scripts/Herder.cs
+++ Magic Sheppard/Assets/Scripts/Herder.cs
@@ -3,20 +3,20 @@ using System.Collections;
 
 public class Herder : MonoBehaviour {
 	float speed = 15;
-	float rotatespeed = 40;
+	float rotatespeed = 10;
 	// Use this for initialization
 	void Start () {
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		if(Input.GetKey(KeyCode.RightArrow))
+        if (Input.GetAxis("Mouse X")>0)
 		{
-			transform.Rotate(new Vector3(0,rotatespeed * Time.deltaTime,0));
+            transform.Rotate(new Vector3(0, rotatespeed * Input.GetAxis("Mouse X"), 0));
 		}
-		if(Input.GetKey(KeyCode.LeftArrow))
+        if (Input.GetAxis("Mouse X")<0)
 		{
-			transform.Rotate(new Vector3(0,-rotatespeed * Time.deltaTime,0));
+			transform.Rotate(new Vector3(0,rotatespeed * Input.GetAxis("Mouse X"),0));
 		}
 		if(Input.GetKey(KeyCode.UpArrow))
 		{
