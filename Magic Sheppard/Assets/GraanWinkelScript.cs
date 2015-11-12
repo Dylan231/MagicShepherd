78274654e3da4210403d314a75ca2a0b48495862
umbrielpost <umbrielpost@gmail.com>
2015-11-12 17:18:06 +0100

    GraanKopenWinkel

---
+++ Magic Sheppard/Assets/GraanWinkelScript.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class GraanWinkelScript : MonoBehaviour {
+    public Text AantalGraanText;
+    public int aantalgraangekocht = 0;
+
+	// Use this for initialization
+	void Start () {
+        SetAantalGraan();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	    if (Input.GetKeyDown("mouse 0"))
+        {
+            aantalgraangekocht = aantalgraangekocht + 1;
+            SetAantalGraan();
+        }
+	}
+
+    void SetAantalGraan()
+    {
+        AantalGraanText.text = "Graan gekocht: " + aantalgraangekocht;
+    }
+}
