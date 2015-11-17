37d700cf9d760d80cc75fa7fc76aab048c3b8633
umbrielpost <umbrielpost@gmail.com>
2015-11-17 12:31:00 +0100

    Hond loopt mooier en naar de herder toe aan het einde

--- Magic Sheppard/Assets/Scripts/HondScript.cs
+++ Magic Sheppard/Assets/Scripts/HondScript.cs
@@ -1,80 +1,156 @@
 ﻿using UnityEngine;
 using System.Collections;
+using UnityEngine.UI;
 
 public class HondScript : MonoBehaviour {
-    float speed = 15;
-    float speed2 = 30;
+    public int aantalhondengekocht = HondWinkelScript.aantalhondgekocht;
+    public Text AantalHondenGekocht;
+    bool aan = false;
+    float xbegin;
+    float zbegin;
+    bool n1 = false;
+    bool n2 = false;
+    bool n3 = false;
+    bool n4 = false;
 
-	// Use this for initialization
-	void Start () {
-	
+    // Use this for initialization
+    void Start () {
+        SetAantalHondenText();
 	}
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKey(KeyCode.Alpha2))
+        if (Input.GetKey(KeyCode.Alpha2) && aantalhondengekocht > 0 && aan == false)
         {
             transform.parent = null;
-            StartCoroutine(TimerHond1());
+            aan = true;
+            n1 = true;
+            aantalhondengekocht = aantalhondengekocht - 1;
+            SetAantalHondenText();
+        }
+        if (n1 == true)
+        {
+            xbegin = gameObject.transform.position.x;
+            zbegin = gameObject.transform.position.z;
+            InvokeRepeating("PlekjeZoeken", 0, 0.2f);
+        }
+        if (n2 == true)
+        {
+            xbegin = gameObject.transform.position.x;
+            zbegin = gameObject.transform.position.z;
+            InvokeRepeating("PlekjeZoeken2", 0, 0.2f);
+        }
+        if (n3 == true)
+        {
+            xbegin = gameObject.transform.position.x;
+            zbegin = gameObject.transform.position.z;
+            InvokeRepeating("PlekjeZoeken3", 0, 0.2f);
+        }
+        if (n4 == true)
+        {
+            xbegin = gameObject.transform.position.x;
+            zbegin = gameObject.transform.position.z;
+            InvokeRepeating("TerugnaarHerder", 0, 0.2f);
         }
     }
 
-    IEnumerator TimerHond1()
+    void PlekjeZoeken()
     {
-        InvokeRepeating("RennenHond", 0, 0.2f);
-        yield return new WaitForSeconds(2);
-        CancelInvoke();
-        StartCoroutine(TimerHond2());
+        float xdesired = xbegin + 0.1f;
+        float zdesired = zbegin + 0.5f;
+
+        float xrichting = xdesired - xbegin;
+        float zrichting = zdesired - zbegin;
+
+        float xnu = gameObject.transform.position.x;
+        float znu = gameObject.transform.position.z;
+
+        transform.Translate(new Vector3(xrichting * Time.deltaTime, 0, zrichting * Time.deltaTime));
+
+        if (Mathf.Abs(xdesired - xnu) < 0.1 && Mathf.Abs(zdesired - znu) < 0.1)
+        {
+            n1 = false;
+            CancelInvoke();
+            n2 = true;
+        }
     }
 
-    IEnumerator TimerHond2()
+    void PlekjeZoeken2()
     {
-        InvokeRepeating("DraaienHondenRennen", 0, 0.2f);
-        yield return new WaitForSeconds(2);
-        CancelInvoke();
-        StartCoroutine(TimerHond3());
+        float xdesired = xbegin - 0.1f;
+        float zdesired = zbegin - 0.5f;
+
+        float xrichting = xdesired - xbegin;
+        float zrichting = zdesired - zbegin;
+
+        float xnu = gameObject.transform.position.x;
+        float znu = gameObject.transform.position.z;
+
+        transform.Translate(new Vector3(xrichting * Time.deltaTime, 0, zrichting * Time.deltaTime));
+
+        if (Mathf.Abs(xdesired - xnu) < 0.1 && Mathf.Abs(zdesired - znu) < 0.1)
+        {
+            n2 = false;
+            CancelInvoke();
+            n3 = true;
+        }
+    }
+    
+    void PlekjeZoeken3()
+    {
+        float xdesired = xbegin + 0.1f;
+        float zdesired = zbegin + 0.5f;
+
+        float xrichting = xdesired - xbegin;
+        float zrichting = zdesired - zbegin;
+
+        float xnu = gameObject.transform.position.x;
+        float znu = gameObject.transform.position.z;
+
+        transform.Translate(new Vector3(xrichting * Time.deltaTime, 0, zrichting * Time.deltaTime));
+
+        if (Mathf.Abs(xdesired - xnu) < 0.1 && Mathf.Abs(zdesired - znu) < 0.1)
+        {
+            n3 = false;
+            CancelInvoke();
+            n4 = true;
+        }
     }
 
-    IEnumerator TimerHond3()
+    void TerugnaarHerder()
     {
-        InvokeRepeating("DraaiHondRen", 0, 0.2f);
-        yield return new WaitForSeconds(2);
-        CancelInvoke();
-        StartCoroutine(TimerHond4());
-    }
+        var H = GameObject.FindGameObjectWithTag("Herder");
 
-    IEnumerator TimerHond4()
-    {
-        InvokeRepeating("DraaienHondenRennen", 0, 0.2f);
-        yield return new WaitForSeconds(2);
-        CancelInvoke();
-        gameObject.SetActive(false);
-        ReturnBeginHond();
-    }
+        float xdesired = H.transform.position.x + 1;
+        float zdesired = H.transform.position.z - 1;
 
-    void RennenHond()
-    {
-        transform.Translate(Vector3.forward * speed * Time.deltaTime);
-    }
+        float xnu = gameObject.transform.position.x;
+        float znu = gameObject.transform.position.z;
 
-    void DraaienHondenRennen()
-    {
-        transform.Translate(Vector3.back * speed2 * Time.deltaTime);
-    }
+        float xrichting = xdesired - xnu;
+        float zrichting = zdesired - znu;
+        
+        transform.Translate(new Vector3(xrichting * Time.deltaTime, 0, zrichting * Time.deltaTime));
 
-    void DraaiHondRen()
-    {
-        transform.Translate(Vector3.forward * speed2 * Time.deltaTime);
+        if (Mathf.Abs(xdesired - xnu) < 0.1 && Mathf.Abs(zdesired - znu) < 0.1)
+        {
+            n4 = false;
+            CancelInvoke();
+            ReturnBeginHond();
+        }
     }
 
     void ReturnBeginHond()
     {
-        gameObject.SetActive(true);
         var H = GameObject.FindGameObjectWithTag("Herder");
-        Vector3 beginplekhond = new Vector3(H.transform.position.x + 1, H.transform.position.y - 1, H.transform.position.z - 1);
-        transform.position = beginplekhond;
         transform.parent = H.transform;
+        aan = false;
     }
- }
+
+    void SetAantalHondenText()
+    {
+        AantalHondenGekocht.text = "Aantal honden: " + aantalhondengekocht;
+    }
+}
 
